76621abf63c2fc96dfbb84d3d6516538011089da
Jonapulse <jonapulse@gmail.com>
2025-08-20 12:36:38 -0600

    Day 3 HW 2

--- Day2/GiveChange/GiveChange/main.cpp
+++ Day2/GiveChange/GiveChange/main.cpp
@@ -12,6 +12,10 @@ int main(int argc, const char * argv[]) {
     
     int centPrice;
     int centsPaid;
+    int quarterStock = 2;
+    int dimeStock = 2;
+    int nickelStock = 2;
+    int pennyStock = 2;
     
     cout << "Enter item price in cents:\n";
     cin >> centPrice;
@@ -36,14 +40,34 @@ int main(int argc, const char * argv[]) {
     cout << "Change = " << change << " cents:\n";
     
     int quarters = change / 25;
+    if(quarters > 2)
+        quarters = 2;
     change -= quarters * 25;
     int dimes = change / 10;
+    if(dimes > 2)
+        dimes = 2;
     change -= dimes * 10;
     int nickels = change / 5;
+    if (nickels > 2)
+        nickels = 2;
     change -= nickels * 5;
     int pennies = change;
+    if (pennies > 2)
+        pennies = 2;
+    change -= pennies;
     
-    cout << " Quarters: " << quarters << "\n Dimes: " << dimes << "\n Nickels: " << nickels << "\n Pennies: " << pennies << "\n";
+    if(change == 0)
+    {
+        cout << " Quarters: " << quarters << "\n Dimes: " << dimes
+            << "\n Nickels: " << nickels << "\n Pennies: " << pennies << "\n";
+    }
+    else
+    {
+        cout << " Unable to return change. Out of coins!\n";
+        return 1;
+    }
+    
+    
     
     return 0;
 }
