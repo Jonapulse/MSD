4b953ec730dd9a5c2e36834f951c02899abe1ab4
Jonapulse <jonapulse@gmail.com>
2025-08-29 09:38:23 -0600

    Day10 - Lab - PointersAndArray printing out args

---
+++ Day10/PointersAndArrays/PointersAndArrays/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  PointersAndArrays
+//
+//  Created by Jonathan Pulsipher on 8/29/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    for(int i= 0; i < argc; i++)
+        std::cout << argv[i] << '\n';
+
+    return 0;
+}
