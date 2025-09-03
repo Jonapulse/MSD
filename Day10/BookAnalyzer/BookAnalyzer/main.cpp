a2a5fe693f1f049d0fb3355681b0a5ebc39b5ac0
Jonapulse <jonapulse@gmail.com>
2025-09-03 10:52:00 -0600

    Day10 HW - Book Analyzer. Implemented but broken.

--- Day10/BookAnalyzer/BookAnalyzer/main.cpp
+++ Day10/BookAnalyzer/BookAnalyzer/main.cpp
@@ -13,28 +13,26 @@ using namespace std;
 
 struct BookInfo
 {
-    std::string title = "";
-    std::string author = "";
+    std::string title = "unknown";
+    std::string author = "unknown";
     int wordCount = 0;
     int charCount = 0;
     std::string shortestWord = "";
     std::string longestWord = "";
     vector<string> keywordContext;
     vector<float> keywordPercent;
-}
+};
 
-int main(int argc, const char * argv[]) {
-    string word;
-
-//    string bookName1 = "./The Strange House.txt";
-    string bookName1 = "./HeroicAirmenAndTheirExploits.txt";
-    
-    std::ifstream myBook(bookName1);
+BookInfo getBookInfo(string fileName, string keyword)
+{
+    std::ifstream myBook(fileName);
     BookInfo bookStats;
+    bool collectingKeywordContext = false;
     
     bool readingTitle = false;
     bool readingAuthor = false;
     std:string previousWord = "";
+    string word;
     
     while(myBook >> word)
     {
@@ -68,16 +66,49 @@ int main(int argc, const char * argv[]) {
         bookStats.wordCount++;
         bookStats.charCount += word.size();
         
+        //Check for keywords
+        if(word == keyword)
+        {
+            bookStats.keywordContext.push_back(previousWord + "  " + word);
+            bookStats.keywordPercent.push_back(bookStats.wordCount);
+            collectingKeywordContext = true;
+        } else if (collectingKeywordContext)
+        {
+            bookStats.keywordContext[bookStats.keywordContext.size() - 1] += " " + word;
+            collectingKeywordContext = false;
+        }
         
+        previousWord = word;
     }
     
-//    string line;
-//    while(getline(myBook, line))
-//    {
-//        cout << line << '\n';
-//    }
+    //Convert keyWordPercents to percentage values
+    //
+    for(int i = 0; i < bookStats.keywordPercent.size(); i++)
+        bookStats.keywordPercent[i] = bookStats.keywordPercent[i] / (float)bookStats.wordCount;
     
     myBook.close();
+    
+    return bookStats;
+}
+
+int main(int argc, const char * argv[]) {
+
+//    string bookName1 = "./The Strange House.txt";
+    string bookName = "./HeroicAirmenAndTheirExploits.txt";
+    string keyWord = "fish";
+    
+    BookInfo bookStats = getBookInfo(bookName, keyWord);
+    
+    cout << "Statistics for " << bookStats.title << " by " << bookStats.author
+        << "\n\tNumber of words: " << std::to_string(bookStats.wordCount)
+        << "\n\tNumber of characters: " << std::to_string(bookStats.charCount)
+        << "\n\tThe shortest words is \"" << bookStats.shortestWord << "\", and the longest words is\"" << bookStats.longestWord;
+    
+    cout << "The word " << keyWord << " appears " << bookStats.keywordContext.size() << " times:\n";
+    for(int i = 0; i < bookStats.keywordContext.size(); i++)
+    {
+        cout << "  at " << bookStats.keywordPercent[i] << "%: \"" << bookStats.keywordContext[i] << "\"";
+    }
     
     return 0;
 }
