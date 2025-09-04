c0491b7515e780ae6c4859902ca545d9f5883b0b
Jonapulse <jonapulse@gmail.com>
2025-09-03 21:11:13 -0600

    Day10 HW - completed issue with printing out whole book

--- Day10/BookAnalyzer/BookAnalyzer/main.cpp
+++ Day10/BookAnalyzer/BookAnalyzer/main.cpp
@@ -19,6 +19,7 @@ struct BookInfo
     int charCount = 0;
     std::string shortestWord = "";
     std::string longestWord = "";
+    std::string keyword = "";
     vector<string> keywordContext;
     vector<float> keywordPercent;
 };
@@ -36,36 +37,28 @@ BookInfo getBookInfo(string fileName, string keyword)
     
     while(myBook >> word)
     {
-        if(previousWord == "Title: " || readingTitle)
+        // Capture title and author
+        //
+        if(previousWord == "Title:" || readingTitle)
         {
             readingTitle = true;
             if(bookStats.title == "unknown")
                 bookStats.title = word;
+            else if (word == "Author:")
+                readingTitle = false;
             else
                 bookStats.title += " " + word;
-        } else if(previousWord == "Author: " || readingAuthor)
+        } else if(previousWord == "Author:" || readingAuthor)
         {
             readingAuthor = true;
             
             if(bookStats.author == "unknown")
                 bookStats.author = word;
+            else if(word == "Release")
+                readingAuthor = false;
             else
                 bookStats.author += " " + word;
             
-        }
-        
-        if(word == "Title:")
-        {
-            readingTitle = true;
-        }
-        else if (word == "Author:")
-        {
-            readingTitle = false;
-            readingAuthor = true;
-        }
-        else if (word == "ReleasedDate:")
-        {
-            readingAuthor = false;
         }
         
         if(bookStats.shortestWord == "" || word.size() < bookStats.shortestWord.size())
@@ -76,10 +69,12 @@ BookInfo getBookInfo(string fileName, string keyword)
         bookStats.wordCount++;
         bookStats.charCount += word.size();
         
-        //Check for keywords
+        //Capture keyword along with the preceding and following word
+        //
+        bookStats.keyword = keyword;
         if(word == keyword)
         {
-            bookStats.keywordContext.push_back(previousWord + "  " + word);
+            bookStats.keywordContext.push_back(previousWord + " " + word);
             bookStats.keywordPercent.push_back(bookStats.wordCount);
             collectingKeywordContext = true;
         } else if (collectingKeywordContext)
@@ -101,24 +96,30 @@ BookInfo getBookInfo(string fileName, string keyword)
     return bookStats;
 }
 
-int main(int argc, const char * argv[]) {
-
-//    string bookName1 = "./The Strange House.txt";
-    string bookName = "./HeroicAirmenAndTheirExploits.txt";
-    string keyWord = "fish";
-    
-    BookInfo bookStats = getBookInfo(bookName, keyWord);
-    
+void printBookInfo(BookInfo bookStats)
+{
     cout << "Statistics for " << bookStats.title << " by " << bookStats.author
         << "\n\tNumber of words: " << std::to_string(bookStats.wordCount)
         << "\n\tNumber of characters: " << std::to_string(bookStats.charCount)
-        << "\n\tThe shortest words is \"" << bookStats.shortestWord << "\", and the longest words is\"" << bookStats.longestWord;
+        << "\n\tThe shortest words is \"" << bookStats.shortestWord << "\", and the longest words is\"" << bookStats.longestWord << '\n';
     
-    cout << "The word " << keyWord << " appears " << bookStats.keywordContext.size() << " times:\n";
+    cout << "The word " << bookStats.keyword << " appears " << bookStats.keywordContext.size() << " times:\n";
     for(int i = 0; i < bookStats.keywordContext.size(); i++)
     {
-        cout << "  at " << bookStats.keywordPercent[i] << "%: \"" << bookStats.keywordContext[i] << "\"";
+        cout << "  at " << bookStats.keywordPercent[i] << "%: \"" << bookStats.keywordContext[i] << "\"\n";
     }
+}
+
+int main(int argc, const char * argv[]) {
+
+    //string bookName = "./The Strange House.txt";
+    string bookName = "./HeroicAirmenAndTheirExploits.txt";
+    //string keyword = "whom";
+    string keyword = "fish";//Note: lone among the words I've tested, this one finds nothing. Ah! I'm not recognizing words with attached punctuation. Further refinement would fix this issue as well as give more meaningful longest words (currently most are hyphenated or urls)
+    
+    printBookInfo(getBookInfo(bookName, keyword));
+    
+    
     
     return 0;
 }
