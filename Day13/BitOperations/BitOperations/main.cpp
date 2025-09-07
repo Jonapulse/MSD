3b901cced0afc80a93398f7d8d53689777db4496
Jonapulse <jonapulse@gmail.com>
2025-09-06 18:59:10 -0600

    Day13 HW

--- Day13/BitOperations/BitOperations/main.cpp
+++ Day13/BitOperations/BitOperations/main.cpp
@@ -142,6 +142,22 @@ uint32_t SetByte( uint32_t input, uint8_t value, int b )
     return returnBytes;
 }
 
+/*
+ * Challenge Question
+ *
+ * Increment
+ * This function should return x + 1 but should only make use of bitwise operators and == or !=
+*/
+int Increment( uint32_t x ){
+    int firstZeroIndex = 0;
+    while(firstZeroIndex < 32 && GetBit(x, firstZeroIndex))
+        firstZeroIndex++;
+    uint32_t incMask = 0xffffffff;
+    //If a 0 was found, create a mask of 0s and 1s, with 1s starting where the 0 was found
+    if(firstZeroIndex < 32)
+        incMask >>= (32 - firstZeroIndex - 1);
+    return x ^ incMask;
+}
 
 /*
  * Negate() computes the negation of an integer.
@@ -169,24 +185,7 @@ uint32_t SetByte( uint32_t input, uint8_t value, int b )
  */
 int Negate( int input )
 {
-    int flippedInput = ~input;
-    if(GetBit(flippedInput, 0))
-        flippedInput = (~input << 1) | 1;
-    return flippedInput | 1;
-}
-
-
-/*
- * Challenge Question
- *
- * Increment
- * This function should return x + 1 but should only make use of bitwise operators and == or !=
-*/
-int Increment( uint32_t x ){
-    if(GetBit(x, 0))
-        return x << 1 | 1;
-    else
-        return x | 1;
+    return Increment(~input);
 }
 
 
@@ -253,55 +252,55 @@ int main()
    * or all ones in a certain byte: 0x00ff0000
    */
   
-//  TestBool( GetBit( 0, 0 ), false, "GetBit1" );
-//  TestBool( GetBit( 0, 1 ), false, "GetBit2" );
-//  TestBool( GetBit( 0, 31 ), false, "GetBit3" );
-//  TestBool( GetBit( -1, 0 ), true, "GetBit4" );
-//  TestBool( GetBit( -1, 1 ), true, "GetBit5" );
-//  TestBool( GetBit( -1, 31 ), true, "GetBit6" );
-//  TestBool( GetBit( 0xAAAAAAAA, 0 ), false, "GetBit7" );
-//  TestBool( GetBit( 0xAAAAAAAA, 16 ), false, "GetBit8" );
-//  TestBool( GetBit( 0xAAAAAAAA, 17 ), true, "GetBit9" );
-//  TestBool( GetBit( 0xAAAAAAAA, 31 ), true, "GetBit10" );
-//
-//  TestBool( IsNegative( 0 ), false, "IsNegative1" );
-//  TestBool( IsNegative( 1 ), false, "IsNegative2" );
-//  TestBool( IsNegative( -1 ), true, "IsNegative3" );
-//  TestBool( IsNegative( 0xAAAAAAAA ), true, "IsNegative4" );
-//  TestBool( IsNegative( 0x55555555 ), false, "IsNegative5" );
-//  TestBool( IsNegative( INT_MIN ), true, "IsNegative6" );
-//  TestBool( IsNegative( INT_MAX ), false, "IsNegative7" );
-//
-//  Test32Bit( NumBitsSet( 0 ), 0, "NumBitsSet1" );
-//  Test32Bit( NumBitsSet( 1 ), 1, "NumBitsSet2" );
-//  Test32Bit( NumBitsSet( 2 ), 1, "NumBitsSet3" );
-//  Test32Bit( NumBitsSet( 3 ), 2, "NumBitsSet4" );
-//  Test32Bit( NumBitsSet( 5 ), 2, "NumBitsSet5" );
-//  Test32Bit( NumBitsSet( -1 ), 32, "NumBitsSet6" );
-//  Test32Bit( NumBitsSet( 0xAAAAAAAA ), 16, "NumBitsSet7" );
-//  Test32Bit( NumBitsSet( 0x55555555 ), 16, "NumBitsSet8" );
-//
-//  Test8Bit( GetByte( 0, 0 ), 0, "GetByte1" );
-//  Test8Bit( GetByte( 0, 1 ), 0, "GetByte2" );
-//  Test8Bit( GetByte( 0, 2 ), 0, "GetByte3" );
-//  Test8Bit( GetByte( 0, 3 ), 0, "GetByte4" );
-//  Test8Bit( GetByte( 0xAAAAAAAA, 0 ), 0xaa, "GetByte4" );
-//  Test8Bit( GetByte( 0xAAAAAAAA, 1 ), 0xaa, "GetByte5" );
-//  Test8Bit( GetByte( 0xAAAAAAAA, 2 ), 0xaa, "GetByte6" );
-//  Test8Bit( GetByte( 0xAAAAAAAA, 3 ), 0xaa, "GetByte7" );
-//  Test8Bit( GetByte( 0x12345678, 0 ), 0x78, "GetByte8" );
-//  Test8Bit( GetByte( 0x12345678, 1 ), 0x56, "GetByte9" );
-//  Test8Bit( GetByte( 0x12345678, 2 ), 0x34, "GetByte10" );
-//  Test8Bit( GetByte( 0x12345678, 3 ), 0x12, "GetByte11" );
-//
-//  Test32Bit( SetByte( 0, 0xFF, 0 ), 0xFF, "SetByte1" );
-//  Test32Bit( SetByte( 0, 0xFF, 1 ), 0xFF00, "SetByte2" );
-//  Test32Bit( SetByte( 0, 0xFF, 2 ), 0xFF0000, "SetByte3" );
-//  Test32Bit( SetByte( 0, 0xFF, 3 ), 0xFF000000, "SetByte4" );
-//  Test32Bit( SetByte( 0x12345678, 0xAA, 0 ), 0x123456aa, "SetByte5" );
-//  Test32Bit( SetByte( 0x12345678, 0xAA, 1 ), 0x1234aa78, "SetByte6" );
-//  Test32Bit( SetByte( 0x12345678, 0xAA, 2 ), 0x12aa5678, "SetByte7" );
-//  Test32Bit( SetByte( 0x12345678, 0xAA, 3 ), 0xaa345678, "SetByte8" );
+  TestBool( GetBit( 0, 0 ), false, "GetBit1" );
+  TestBool( GetBit( 0, 1 ), false, "GetBit2" );
+  TestBool( GetBit( 0, 31 ), false, "GetBit3" );
+  TestBool( GetBit( -1, 0 ), true, "GetBit4" );
+  TestBool( GetBit( -1, 1 ), true, "GetBit5" );
+  TestBool( GetBit( -1, 31 ), true, "GetBit6" );
+  TestBool( GetBit( 0xAAAAAAAA, 0 ), false, "GetBit7" );
+  TestBool( GetBit( 0xAAAAAAAA, 16 ), false, "GetBit8" );
+  TestBool( GetBit( 0xAAAAAAAA, 17 ), true, "GetBit9" );
+  TestBool( GetBit( 0xAAAAAAAA, 31 ), true, "GetBit10" );
+
+  TestBool( IsNegative( 0 ), false, "IsNegative1" );
+  TestBool( IsNegative( 1 ), false, "IsNegative2" );
+  TestBool( IsNegative( -1 ), true, "IsNegative3" );
+  TestBool( IsNegative( 0xAAAAAAAA ), true, "IsNegative4" );
+  TestBool( IsNegative( 0x55555555 ), false, "IsNegative5" );
+  TestBool( IsNegative( INT_MIN ), true, "IsNegative6" );
+  TestBool( IsNegative( INT_MAX ), false, "IsNegative7" );
+
+  Test32Bit( NumBitsSet( 0 ), 0, "NumBitsSet1" );
+  Test32Bit( NumBitsSet( 1 ), 1, "NumBitsSet2" );
+  Test32Bit( NumBitsSet( 2 ), 1, "NumBitsSet3" );
+  Test32Bit( NumBitsSet( 3 ), 2, "NumBitsSet4" );
+  Test32Bit( NumBitsSet( 5 ), 2, "NumBitsSet5" );
+  Test32Bit( NumBitsSet( -1 ), 32, "NumBitsSet6" );
+  Test32Bit( NumBitsSet( 0xAAAAAAAA ), 16, "NumBitsSet7" );
+  Test32Bit( NumBitsSet( 0x55555555 ), 16, "NumBitsSet8" );
+
+  Test8Bit( GetByte( 0, 0 ), 0, "GetByte1" );
+  Test8Bit( GetByte( 0, 1 ), 0, "GetByte2" );
+  Test8Bit( GetByte( 0, 2 ), 0, "GetByte3" );
+  Test8Bit( GetByte( 0, 3 ), 0, "GetByte4" );
+  Test8Bit( GetByte( 0xAAAAAAAA, 0 ), 0xaa, "GetByte4" );
+  Test8Bit( GetByte( 0xAAAAAAAA, 1 ), 0xaa, "GetByte5" );
+  Test8Bit( GetByte( 0xAAAAAAAA, 2 ), 0xaa, "GetByte6" );
+  Test8Bit( GetByte( 0xAAAAAAAA, 3 ), 0xaa, "GetByte7" );
+  Test8Bit( GetByte( 0x12345678, 0 ), 0x78, "GetByte8" );
+  Test8Bit( GetByte( 0x12345678, 1 ), 0x56, "GetByte9" );
+  Test8Bit( GetByte( 0x12345678, 2 ), 0x34, "GetByte10" );
+  Test8Bit( GetByte( 0x12345678, 3 ), 0x12, "GetByte11" );
+
+  Test32Bit( SetByte( 0, 0xFF, 0 ), 0xFF, "SetByte1" );
+  Test32Bit( SetByte( 0, 0xFF, 1 ), 0xFF00, "SetByte2" );
+  Test32Bit( SetByte( 0, 0xFF, 2 ), 0xFF0000, "SetByte3" );
+  Test32Bit( SetByte( 0, 0xFF, 3 ), 0xFF000000, "SetByte4" );
+  Test32Bit( SetByte( 0x12345678, 0xAA, 0 ), 0x123456aa, "SetByte5" );
+  Test32Bit( SetByte( 0x12345678, 0xAA, 1 ), 0x1234aa78, "SetByte6" );
+  Test32Bit( SetByte( 0x12345678, 0xAA, 2 ), 0x12aa5678, "SetByte7" );
+  Test32Bit( SetByte( 0x12345678, 0xAA, 3 ), 0xaa345678, "SetByte8" );
 
   Test32Bit( Negate( -1 ), 1, "Negate1" );
   Test32Bit( Negate( 1 ), -1, "Negate2" );
