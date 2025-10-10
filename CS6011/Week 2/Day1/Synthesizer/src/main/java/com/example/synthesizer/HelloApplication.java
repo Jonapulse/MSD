a686c7be739bc0c2578b4267bf29a183824c0973
Jonapulse <jonapulse@gmail.com>
2025-10-09 23:32:48 -0600

    Linear Ramp rough in (not testing yet)

--- CS6011/Week 2/Day1/Synthesizer/src/main/java/com/example/synthesizer/HelloApplication.java
+++ CS6011/Week 2/Day1/Synthesizer/src/main/java/com/example/synthesizer/HelloApplication.java
@@ -1,8 +1,12 @@
 package com.example.synthesizer;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +14,38 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        //todo: All these warnings - if they're causing problems might be the 25 version thing
+
+        //In class stuff
+        AnchorPane ap = new AnchorPane();
+
+        VBox menu = new VBox(10);
+        menu.setStyle("-fx-background-color: #f0f0f0;");
+        Button swBtn = new Button("Create Sine Wave");
+        Button vcBtn = new Button("Create Volume Control");
+        menu.getChildren().addAll(swBtn, vcBtn);
+
+        swBtn.setOnAction(e -> makeSineWave(ap));
+
+        ap.getChildren().add(menu);
+
+        Scene scene = new Scene(ap, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private int swcNumber = 0;
+    private void makeSineWave(AnchorPane ap) {
+        HBox swBox = new HBox(10);
+        Text name = new Text("Sine Wave");
+        swBox.getChildren().add(name);
+        Button swBtn = new Button("Sine Wave");
+        swBox.getChildren().add(swBtn);
+        ap.getChildren().add(swBtn);
+        swBox.setLayoutX(200 + swcNumber++ * 100);
+        swBox.setLayoutY(200);
+        //How do we get it to show up on the screen?
     }
 }
---
+++ CS6011/Week 2/Day1/Synthesizer/src/main/java/com/example/synthesizer/LinearRamp.java
@@ -0,0 +1,27 @@
+package com.example.synthesizer;
+
+public class LinearRamp implements AudioComponent {
+    @Override
+    public AudioClip getClip() {
+        return clip_;
+    }
+
+    @Override
+    public boolean hasInput() {
+        return false;
+    }
+
+    @Override
+    public void connectInput(AudioComponent input) {
+        assert(false); //This Component does not accept input
+    }
+
+    private AudioClip clip_;
+    public LinearRamp(int start, int stop){
+        clip_ = new AudioClip();
+        int numSamples = (int)(AudioClip.SAMPLE_RATE * AudioClip.DURATION);
+        for(int i = 0; i < numSamples; i++) {
+            clip_.setSample(i, (int) Math.clamp(((1f * start * (numSamples - i) + stop * i) / numSamples), Short.MIN_VALUE, Short.MAX_VALUE));
+        }
+    }
+}
