f085ccded6772e7ac53b6f7e1181607c22ed3dbc
Jonapulse <jonapulse@gmail.com>
2025-10-09 23:41:54 -0600

    VFSineWave Rough Draft implementation (I'll test it after I board the flight, MOM)

---
+++ CS6011/Week 2/Day1/Synthesizer/src/main/java/com/example/synthesizer/VFSineWave.java
@@ -0,0 +1,35 @@
+package com.example.synthesizer;
+
+public class VFSineWave implements AudioComponent {
+
+    // AudioComponent Implementations
+    //
+    @Override
+    public AudioClip getClip() {
+        return clip_;
+    }
+
+    @Override
+    public boolean hasInput() {
+        return input_ != null;
+    }
+
+    @Override
+    public void connectInput(AudioComponent input) {
+        input_ = input;
+        int phase = 0;
+        AudioClip inputClip = input_.getClip();
+        for(int i = 0; i < (int)(AudioClip.SAMPLE_RATE * AudioClip.DURATION); i++) {
+            phase += 2 * Math.PI * inputClip.getSample(i) / AudioClip.SAMPLE_RATE;
+            clip_.setSample(i, (int) (Short.MAX_VALUE * Math.sin(phase)));
+        }
+    }
+
+    // VFSineWave
+    //
+    private AudioClip clip_;
+    private AudioComponent input_;
+    VFSineWave() {
+        clip_ = new AudioClip(); //Stays empty until input connected
+    }
+}
