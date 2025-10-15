161e2cc4d0c1dce312f89d04d41d28d13ed59142
Jonapulse <jonapulse@gmail.com>
2025-10-15 16:04:35 -0600

    Synthesizer: extending AudioComponentWidget with SineWaveWidget

--- CS6011/Week3/Day12/myPageBuilder.js
+++ CS6011/Week3/Day12/myPageBuilder.js
@@ -1,22 +1,56 @@
+
+let colLabel = null;
+ let rowLabel = null;
+
+
 function main(){
     
     let mTable = document.createElement('table');
     document.body.appendChild(mTable);
 
     let multRange = 10;
-    for( let i = 1; i <= multRange; i++)
+
+    for( let i = 0; i <= multRange; i++)
     {
         let mRow = document.createElement('tr');
+        
         mTable.appendChild(mRow);
-        for( let j = 1; j <= multRange; j++)
+        for( let j = 0; j <= multRange; j++)
         {
+
+            
             let mData = document.createElement('td');
-            mData.textContent = String(i * j);
+            if(i == 0){
+                mData.className = "multLabel";
+                mData.textContent = j == 0 ? "" : "* " + String(j); //For i and j both zero, this stays empty 
+                colLabel = mData;
+            } 
+            else if(j == 0){
+                mData.className = "multLabel";
+                mData.textContent = "* " + String(i);
+                rowLabel = mData;
+            }
+            else {
+                mData.className = "multData";
+                mData.textContent = String(i * j);
+                mData.addEventListener("click", (mData) => {
+                      //console.log(colLabel);
+
+                    console.log(j);
+                    console.log(i);
+                    mData.className += "multDataChange";
+                    colLabel.className = "focused";
+                    // rowLabel.className = "focused";
+                   console.log("Do I even exist?");
+                })
+            }
             mRow.appendChild(mData);
         }
     }
 
-    console.log("Hello JS!");
+    let chaosButton = document.createElement('button');
+    chaosButton.textContent = "No, not the...";
+    document.body.appendChild(chaosButton);
 }
 
 main();
