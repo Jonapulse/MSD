b004f24b8f78bba48d0cf11f7c0f6718010bb4a8
Jonah Pulsipher <jonapulse@gmail.com>
2025-11-12 16:15:14 -0700

    A4 pseudocode, test class, and rough draft merge implementation

--- CS6012/Week2/Day6/A4Sorting/src/assignment04/SortUtil.java
+++ CS6012/Week2/Day6/A4Sorting/src/assignment04/SortUtil.java
@@ -9,15 +9,166 @@ public class SortUtil {
 
     final int SMALL_SORT_THRESHOLD = 100;
 
+    ///////////////////////
+    /// MergeSort & Helpers
+    /**
+     * Sorts list with mergeSort method, using provided comprator
+     * Driver method
+     *
+     * @param list
+     * @param comparator
+     * @param <T>
+     */
     public static <T> void mergesort(ArrayList<T> list, Comparator<? super T> comparator)
     {
-        //MergeSort. Should switch to insertion sort when the size of the sublist to be sorted is small enough.
+        if(comparator == null)
+            comparator = (Comparator<? super T>) Comparator.naturalOrder();
+
+        //TODO: pre-allocate the temp array to use for this
+        //mergeSortRecurse
     }
 
+
+    static <T> void mergeSortRecurse(ArrayList<T> list, ArrayList<T> out, int b, int e, Comparator<? super T> comparator)
+    {
+        //if arr.length < 2
+            // return;
+
+        //if left > threshold
+        // mergeSort(left)
+        //else
+        //insertion sort
+        //if right > threshold
+       // mergeSort(right)
+        //else
+        //insertion sort
+
+       // merge(arr, left, right)
+        //? Is this where we copy back from scratch? Think through it functionally. Yeah.
+    }
+
+    /**
+     *
+     * @param out
+     * @param beg  - begin
+     * @param endL - final index of L. 'beginR' == endL + 1;
+     * @param endR - final index of R.
+     */
+    static <T> void merge(ArrayList<T> in, ArrayList<T> out, int beg, int endL, int endR, Comparator<? super T> comparator){
+       int i = 0;
+       int l = beg;
+       int r = endL + 1;
+
+       //Loop until l or r runs out
+         //
+       while(l < endL - beg && r < endR - endL){
+           if(comparator.compare(in.get(l), out.get(r)) <= 0){
+               out.set(i, in.get(l));
+               i++; l++;
+           }
+           else
+           {
+               out.set(i, out.get(r));
+               i++; r++;
+           }
+       }
+
+       //fill remaining ls or rs
+         //
+        for( ; l < endL - beg; l++, i++)
+            out.set(i, in.get(l));
+        for( ; r < endR - endL; r++, i++)
+            out.set(i, out.get(r));
+    }
+
+    /**
+     *
+     * @param list
+     * @param b - begin index
+     * @param e - end index
+     * @return
+     */
+    public static <T> ArrayList<T> insertionSort(ArrayList<T> list, int b, int e)
+    {
+        return null;
+    }
+
+    ///////////////////////
+    /// QuickSort & Helpers
+    /**
+     *
+     * @param list
+     * @param comparator
+     */
     public static <T> void quicksort(ArrayList<T> list, Comparator<? super T> comparator){
-
+        //
     }
 
+    /**
+     *
+     * @param list
+     * @param b
+     * @param e
+     * @param comparator
+     */
+    static <T> void quicksortRecurse(ArrayList<T> list, int b, int e, Comparator<? super T> comparator){
+        //partition the list
+        //quicksort left
+        //quicksort right
+    }
+
+    /**
+     *
+     * @param list
+     * @param pivot
+     * @return index of pivot, an index all values earlier in the array are smaller, and all later are larger
+     */
+    static <T> int partition(ArrayList<T> list, int pivot, Comparator<? super T> comparator)
+    {
+        //swap pivot to end
+        //left = 0;
+        //right = end - 1//to not point at pivot
+        //while(left < right)
+            //while(arr[left < pivot)
+                //left++
+            //while(arr[right > pivot)
+                //right--;
+            //swap left/right
+            //left++ right--
+        //swap left and pivot
+        return -1;
+    }
+
+    /**
+     * Approximates the median of a list by taking the median of values at start, middle, and end
+     *
+     * @param list
+     * @return index of the median
+     */
+    private <T> int medianOf3(ArrayList<T> list){
+        //TODO: Figure out how to do this without sort, and with using those funky Ts
+        return -1;
+    }
+
+    /**
+     * Approximates the median of a list by taking the median of the "medianOf3" from each third of the list
+     *
+     * @param list
+     * @return index of the median
+     */
+    private <T> int medianOf9(ArrayList<T> list)
+    {
+        if(list.size() < 9)
+            return medianOf3(list);
+
+
+        //get 9 random samples.
+        //Try to minimize the median for 3 lists of 3 and do that on the forth
+        return -1;
+    }
+
+    /////////////////////
+    /// Case Generation
     public static ArrayList<Integer> generateBestCase(int size)
     {
         ArrayList<Integer> list = new ArrayList<>();
@@ -55,36 +206,4 @@ public class SortUtil {
         }
         return list;
     }
-
-
-
-
-    /**
-     * Approximates the median of a list by taking the median of values at start, middle, and end
-     *
-     * @param list
-     * @return index of the median
-     */
-    private <T> int medianOf3(ArrayList<T> list){
-        //TODO: Figure out how to do this without sort, and with using those funky Ts
-        return -1;
-    }
-
-    /**
-     * Approximates the median of a list by taking the median of the "medianOf3" from each third of the list
-     *
-     * @param list
-     * @return index of the median
-     */
-    private <T> int medianOf9(ArrayList<T> list)
-    {
-        if(list.size() < 9)
-            return medianOf3(list);
-
-
-        //get 9 random samples.
-        //Try to minimize the median for 3 lists of 3 and do that on the forth
-        return -1;
-    }
-
 }
---
+++ CS6012/Week2/Day6/A4Sorting/src/assignment04/SortUtilTest.java
@@ -0,0 +1,26 @@
+package assignment04;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortUtilTest {
+
+    @org.junit.jupiter.api.Test
+    void mergesort() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void merge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insertionSort() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void quicksort() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void partition() {
+    }
+}
