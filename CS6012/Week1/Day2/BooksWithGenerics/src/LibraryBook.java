3c80e07d3c9fd810c0e3ec9e742b575fb3610eae
Jonah Pulsipher <jonapulse@gmail.com>
2025-11-13 15:48:28 -0700

    A2: starting classes and basic LibraryBook

---
+++ CS6012/Week1/Day2/BooksWithGenerics/src/LibraryBook.java
@@ -0,0 +1,29 @@
+import java.util.GregorianCalendar;
+
+public class LibraryBook extends Book {
+
+    ///Fields holder and dueDate are null when the book is not checked out
+    String holder = null;
+    GregorianCalendar dueDate = null;
+
+    public LibraryBook(long isbn, String author, String title) {
+        super(isbn, author, title);
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public GregorianCalendar getDueDate() {
+        return dueDate;
+    }
+    public void checkOut(String holder, GregorianCalendar dueDate) {
+        this.holder = holder;
+        this.dueDate = dueDate;
+    }
+
+    public void checkIn() {
+        this.holder = null;
+        this.dueDate = null;
+    }
+}
