16df2980409999ce71f2cc166f9af113de06ee06
Jonah Pulsipher <jonapulse@gmail.com>
2025-11-19 19:55:53 -0700

    A6: BinarySearchTree fully implemented and passing basic tests

--- CS6012/Week2/Day6/A4Sorting/src/SortTimingAnalysis.java
+++ CS6012/Week2/Day6/A4Sorting/src/SortTimingAnalysis.java
@@ -10,8 +10,9 @@ public class SortTimingAnalysis {
 
         int[] problemSizes = {(int)Math.pow(2, 10), (int)Math.pow(2,11), (int)Math.pow(2,12), (int)Math.pow(2,13),
                 (int)Math.pow(2,14),(int)Math.pow(2,15), (int)Math.pow(2,16), (int)Math.pow(2,17), (int)Math.pow(2,18),
-                (int)Math.pow(2,19), (int)Math.pow(2,20)};
-        TimingTest t = new TimingTest(problemSizes, 10);
+                (int)Math.pow(2,19), (int)Math.pow(2,20),
+        (int)Math.pow(2,21), (int)Math.pow(2,22), (int)Math.pow(2,23),};
+        TimingTest t = new TimingTest(problemSizes, 100);
         var results = t.run();
 
         System.out.println("n, time");
