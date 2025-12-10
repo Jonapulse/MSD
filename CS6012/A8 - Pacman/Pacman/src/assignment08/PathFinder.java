358a03a4ead3763a227bdab4c1eb51b59fae7bb2
Jonah Pulsipher <jonapulse@gmail.com>
2025-12-10 13:31:26 -0700

    Starting pacman

---
+++ CS6012/A8 - Pacman/Pacman/src/assignment08/PathFinder.java
@@ -0,0 +1,9 @@
+package assignment08;
+
+public class PathFinder {
+    public static void solveMaze(String inputFile, String outputFile){
+        //method for reading the maze
+        //method for solving the maze
+        //method for outputting the solution
+    }
+}
---
+++ CS6012/A8 - Pacman/Pacman/src/assignment08/PathFinderTest.java
@@ -0,0 +1,47 @@
+package assignment08;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PathFinderTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void readMaze(){
+
+    }
+
+    @Test
+    void processMaze(){
+
+    }
+
+    @Test
+    void outputSolution(){
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void solveMaze() {
+    }
+
+    @Test
+    void solveMazeCant(){
+
+    }
+
+    @Test
+    void solveMazeEmpty() {
+    }
+
+    @Test
+    void solveMazeTooSmall() {
+    }
+
+    @Test
+    void solveMazeHuge() {}
+}
