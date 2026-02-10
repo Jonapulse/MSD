b0c0213f59e2d228cafab5535f2204f28ef6fd4c
Jonapulse <jonapulse@gmail.com>
2026-02-10 08:25:30 -0700

    CS6015 Shell - args being read, but processes exit with failure (though they run correctly)

--- Spring2026/CS6013/Week6/UnixShell/jshell.cpp
+++ Spring2026/CS6013/Week6/UnixShell/jshell.cpp
@@ -7,34 +7,45 @@ void runShell(){
     while(true){
         cout << "(>'-')> ";
         string command_str;
-        cin >> command_str;
+        getline(cin, command_str);
 
-        vector<Command> commands = getCommands(tokenize(command_str));
+        vector<string> tokens = tokenize(command_str);
+        vector<Command> commands = getCommands(tokens);
         for(Command comm : commands){
+            if(comm.execName == "exit")
+            {
+                cout << "Exiting shell...\n";
+                return;
+            }
+
             int rc = fork();
             if(rc == 0){ 
                 //Child forks and executes
                  //
-                if(comm.inputFd != STDIN_FILENO)
-                    dup2(STDIN_FILENO, comm.inputFd);
-                if(comm.outputFd != STDOUT_FILENO)
-                    dup2(STDOUT_FILENO, comm.outputFd);
-                
-                execvp(comm.execName.c_str(), const_cast<char**>(comm.argv.data()));
+                if(comm.inputFd != STDIN_FILENO){
+                    dup2(comm.inputFd, STDIN_FILENO);
+                    close(comm.inputFd); //You haven't tested dup and close yet. This is what ai summary told you to do for a different request.
+                }
+                if(comm.outputFd != STDOUT_FILENO){
+                    dup2(comm.outputFd, STDOUT_FILENO);
+                    close(comm.outputFd); //You haven't tested dup and close yet. This is what ai summary told you to do for a different request.
+                }
 
+                execvp(comm.execName.c_str(), const_cast<char**>(comm.argv.data()));
+                
             }
             else 
             {
                 //Parent waits for child to die
                  //
                 int status;
-                waitpid(rc, &status, 0); 
-                if(status != 0)
+                pid_t terminated_pid = waitpid(rc, &status, 0); 
+                if(terminated_pid == -1)
                 {
-                    string errorStr = "Error while running " + comm.execName + " with status code " + to_string(status);
-                    perror(errorStr.c_str());
-                    //TODO: Clean up open file descriptors
+                    perror("waitPid failed");
+                    exit(EXIT_FAILURE);
                 }
+                //TODO: clean up open file descriptors
             }
         }
     }
--- Spring2026/CS6013/Week6/UnixShell/main.cpp
+++ Spring2026/CS6013/Week6/UnixShell/main.cpp
@@ -3,11 +3,6 @@
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "READY\n";
-    std::string s;
-    std::cin >> s;
-    std::cout << "GOT: " << s << "\n";
-
     runShell();
     return 0;
 }
