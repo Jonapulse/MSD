fd8a39bb487e882080162f07bd6b7b9ce913a9a5
Jonapulse <jonapulse@gmail.com>
2026-02-09 19:34:29 -0700

    CS6013 - cleaning up vscode to allow IDE debuggin

--- Spring2026/CS6013/Week6/UnixShell/main.cpp
+++ Spring2026/CS6013/Week6/UnixShell/main.cpp
@@ -1,7 +1,13 @@
 #include "jshell.hpp"
+#include <iostream>
 
 int main(int argc, char const *argv[])
 {
+    std::cout << "READY\n";
+    std::string s;
+    std::cin >> s;
+    std::cout << "GOT: " << s << "\n";
+
     runShell();
     return 0;
 }
