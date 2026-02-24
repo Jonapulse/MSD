706a718637610289f592281a0a36d9363bf85ade
Jonapulse <jonapulse@gmail.com>
2026-02-24 15:10:03 -0700

    CS 6014 - (bad) block cipher encrypting and decrypting

--- Spring2026/CS6014/Week9/cipher.py
+++ Spring2026/CS6014/Week9/cipher.py
@@ -30,21 +30,49 @@ def fisher_yates_shuffle(arr):
         arr[i], arr[j] = arr[j], arr[i]
     return arr
 
+def bitshift_wrap(data, goLeft):
+    n = int.from_bytes(data, byteorder='big')
+    bit_count = len(data) * 8
+    if goLeft:
+        shifted = ((n << 1) | (n >> (bit_count - 1))) & ((1 << bit_count) - 1) #the & gives us a 64-bit mask of 1s to trim back down to the original size of data
+    else: 
+        shifted = ((n >> 1) | (n << (bit_count - 1))) & ((1 << bit_count) - 1)
+    return list(shifted.to_bytes(len(data), byteorder='big'))
+
 def encrypt_message(message, sub_tables, key, num_rounds):
     alg_state = [ord(c) for c in message[0:8]]
-    for i in range(num_rounds):
-        alg_state = [alg_byte ^ key_byte for alg_byte, key_byte in zip(alg_state, key)]
-        for j in range(8):
-            pass
-    pass
+    for round in range(num_rounds):
+        #xor with key
+        alg_state = [alg_byte ^ key_byte for alg_byte, key_byte in zip(alg_state, key)] 
+        #substitute from table
+        for i in range(8):
+            alg_state[i] = sub_tables[i][alg_state[i]] 
+        #rotate state 1 bit to the left
+        alg_state = bitshift_wrap(alg_state, True)
+    ciphertext = ""
+    for c in alg_state:
+        ciphertext += chr(c)
+    return ciphertext; #this only works for the first 8 characters. I think I'd need to do repeat every 8 chars to make this a true blue cipher? And we're intentionally not changing the key?
 
-def decrypt_message(key, sub_tables, message, num_rounds):
-    pass
+def decrypt_message(ciphertext, sub_tables, key, num_rounds):
+    alg_state = [ord(c) for c in ciphertext[0:8]]
+    for round in range(num_rounds):
+        #rotate state 1 bit to the right
+        alg_state = bitshift_wrap(alg_state, False)
+        #substitute backwards from table
+        for i in range(8):
+            alg_state[i] = sub_tables[i].index(alg_state[i])
+        #xor with key
+        alg_state = [alg_byte ^ key_byte for alg_byte, key_byte in zip(alg_state, key)] 
+    plaintext = ""
+    for c in alg_state:
+        plaintext += chr(c)
+    return plaintext
 
 def run():
     key = key_from_pw("password123")
     sub_tables = get_sub_table_of_tables()
-    message = "Let's test to see if this works"
+    message = "Hello Mr"
     ciphertext = encrypt_message(message, sub_tables, key, 16)
     print("Ciphertext is " + str(ciphertext))
     decipheredtext = decrypt_message(ciphertext, sub_tables, key, 16)
